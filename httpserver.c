ac491a5f55d763991e8637496bc4f20c6aaa8d5f
hantaohuang <2812240764@qq.com>
2017-12-06 14:12:26 +0800

    update

--- httpserver.c
+++ httpserver.c
@@ -66,7 +66,7 @@ int main( int argc, char const *argv[] ) {
 
 				setnonblocking(client_sock);
                 // 往线程池中的任务队列里面添加任务
-            	if ( threadpool_add( pool, &accept_request, (void*)&events[i].data.fd, 0 ) != 0 ) { // 添加一个任务到线程池结构中的任务队列里面
+            	if ( threadpool_add( pool, &accept_request, (void*)&client_sock, 0 ) != 0 ) { // 添加一个任务到线程池结构中的任务队列里面
 					printf( "Job add error." );
 				}
         	}
